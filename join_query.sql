2b26310d66737d3d0198604c000d2a3a8bfbd710
Forklein <giuseppepisani2@gmail.com>
2021-09-29 17:21:44 +0200

    Set ex. 5-6

--- join_query.sql
+++ join_query.sql
@@ -30,7 +30,16 @@ ORDER BY `students`.`name`, `students`.`surname` ASC
 
 
 --# 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `degrees`.`name`, `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
 
 
 --# 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
