b1e4851c833b61bce644bc4f46130d9a2c5d13b2
Forklein <giuseppepisani2@gmail.com>
2021-09-29 16:46:15 +0200

    Set ex.4

--- join_query.sql
+++ join_query.sql
@@ -20,6 +20,15 @@ ON `course_teacher`.`course_id` = `courses`.`id`
 WHERE `teacher_id` = 44;
 
 --# 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name`
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+ORDER BY `students`.`name`, `students`.`surname` ASC
+
+
 --# 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 --# 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 --# 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
